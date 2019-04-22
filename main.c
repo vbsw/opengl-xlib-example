de1343a06b7ec128ba1a95dce285b178b82dc414
Vitali Baumtrok <vbsw@mailbox.org>
2019-04-22 13:36:03 +0200

    simplified

--- main.c
+++ main.c
@@ -22,82 +22,6 @@ static Window     window_id;
 static Atom       wm_protocols;
 static Atom       wm_delete_window_atom;
 
-static void close_window ( void ) {
-	XEvent close_event;
-
-	close_event.xclient.type         = ClientMessage;
-	close_event.xclient.message_type = wm_protocols;
-	close_event.xclient.format       = 32;
-	close_event.xclient.data.l[0]    = wm_delete_window_atom;
-	close_event.xclient.data.l[1]    = CurrentTime;
-
-	XSendEvent(display_ptr,window_id,False,NoEventMask,&close_event);
-}
-
-static void resize_frame () {
-	glViewport(0,0,window_width,window_height);
-}
-
-static void draw_graphics ( void ) {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_TRIANGLES);
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex2i(0, 1);
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex2i(-1, -1);
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex2i(1, -1);
-	glEnd();
-	glFlush();
-}
-
-static void process_event ( void ) {
-	XEvent xevent;
-	XNextEvent(display_ptr,&xevent);
-
-	/* printf("event type %d\n", (int) xevent.type); */
-
-	/* input */
-	if (xevent.type==KeyPress) {
-
-		/* printf("key pressed %d\n", (int) xevent.xkey.keycode); */
-
-		/* ESC key */
-		if ((int) xevent.xkey.keycode == 9) {
-			close_window();
-		}
-
-	/* resize */
-	} else if ( xevent.type == ConfigureNotify ) {
-
-		if ( xevent.xconfigure.width  != window_width
-		  || xevent.xconfigure.height != window_height ) {
-
-			window_width  = xevent.xconfigure.width;
-			window_height = xevent.xconfigure.height;
-
-			resize_frame();
-		}
-
-	/* redraw */
-	} else if ( xevent.type          == Expose
-	         && xevent.xexpose.count == 0 ) {
-
-		draw_graphics();
-
-	/* close window */
-	} else if ( xevent.type                 == ClientMessage
-	         && xevent.xclient.message_type == wm_protocols
-	         && xevent.xclient.data.l[0]    == wm_delete_window_atom ) {
-
-		glXMakeCurrent(display_ptr, None, NULL);
-		glXDestroyContext(display_ptr, glxContext);
-		XDestroyWindow(display_ptr,window_id);
-		XCloseDisplay(display_ptr);
-		display_ptr = NULL;
-	}
-}
-
 void create_window ( void ) {
 	int          screen_id            = 0;
 	Screen      *screen_ptr           = NULL;
@@ -152,6 +76,67 @@ void create_window ( void ) {
 
 	} else {
 		printf("error: visual setting not available\n");
+		XCloseDisplay(display_ptr);
+		display_ptr = NULL;
+	}
+}
+
+static void process_event ( void ) {
+	XEvent xevent;
+	XNextEvent(display_ptr,&xevent);
+
+	/* input */
+	if (xevent.type==KeyPress) {
+
+		/* ESC key: close window */
+		if ((int) xevent.xkey.keycode == 9) {
+
+			XEvent close_event;
+
+			close_event.xclient.type         = ClientMessage;
+			close_event.xclient.message_type = wm_protocols;
+			close_event.xclient.format       = 32;
+			close_event.xclient.data.l[0]    = wm_delete_window_atom;
+			close_event.xclient.data.l[1]    = CurrentTime;
+
+			XSendEvent(display_ptr,window_id,False,NoEventMask,&close_event);
+		}
+
+	/* resize */
+	} else if ( xevent.type == ConfigureNotify ) {
+
+		if ( xevent.xconfigure.width  != window_width
+		  || xevent.xconfigure.height != window_height ) {
+
+			window_width  = xevent.xconfigure.width;
+			window_height = xevent.xconfigure.height;
+
+			glViewport(0,0,window_width,window_height);
+		}
+
+	/* draw graphics */
+	} else if ( xevent.type          == Expose
+	         && xevent.xexpose.count == 0 ) {
+
+		glClear(GL_COLOR_BUFFER_BIT);
+		glBegin(GL_TRIANGLES);
+			glColor3f(1.0f, 0.0f, 0.0f);
+			glVertex2i(0, 1);
+			glColor3f(0.0f, 1.0f, 0.0f);
+			glVertex2i(-1, -1);
+			glColor3f(0.0f, 0.0f, 1.0f);
+			glVertex2i(1, -1);
+		glEnd();
+		glFlush();
+
+	/* close window */
+	} else if ( xevent.type                 == ClientMessage
+	         && xevent.xclient.message_type == wm_protocols
+	         && xevent.xclient.data.l[0]    == wm_delete_window_atom ) {
+
+		glXMakeCurrent(display_ptr, None, NULL);
+		glXDestroyContext(display_ptr, glxContext);
+		XDestroyWindow(display_ptr,window_id);
 		XCloseDisplay(display_ptr);
 		display_ptr = NULL;
 	}
